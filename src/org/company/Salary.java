9cd79eb8813040b4b5bb78e37d231b285e75dcc2
venky4498 <venky21798@gmail.com>
2019-11-28 08:52:38 -0800

    asdfg

---
+++ src/org/company/Salary.java
@@ -0,0 +1,15 @@
+package org.company;
+
+public class Salary {
+private void emp1() {
+	System.out.println("xxx");
+}
+private void salary() {
+	System.out.println(1000);
+}
+public static void main(String[] args) {
+	Salary s = new Salary();
+	s.emp1();
+	s.salary();
+}
+}
